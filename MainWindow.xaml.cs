8dae2a2c180ce24f86a1ed78c0adcd80ded7620e
positron48 <positron48@gmail.com>
2015-01-30 19:53:34 +0400

    v 1.0
    подключение к SQLLite, запрос данных из БД и вывод в датагрид

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace CrimesAndIncidents
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqliteWorker sqlWorker;
+        DataTable data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                sqlWorker = new SqliteWorker("CrimesAndIncidents");
+                data = sqlWorker.selectData("SELECT name, number FROM MilitaryUnit");
+                crimesDataGrid.ItemsSource = data.DefaultView;
+                crimesDataGrid.CanUserAddRows = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Во время закгрузки приложения возникли неполадки:\n" + ex.Message);
+            }
+        }
+
+        private void MenuExit_Click_1(object sender, RoutedEventArgs e)
+        {
+            //запрос на сохранение изменений (если вносились)
+            this.Close();
+        }
+    }
+}
---
+++ sqliteWorker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace CrimesAndIncidents
+{
+    class SqliteWorker
+    {
+        SQLiteConnection dbConnection;
+
+        public SqliteWorker(string dbFilePath)
+        {
+            if(dbFilePath!="")
+                try
+                {
+                    dbConnection = new SQLiteConnection("Data Source=" + dbFilePath + ";Version=3;");
+                    dbConnection.Open();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Ошибка чтения файла базы данных \n" + ex.Message);
+                }
+        }
+
+        public void executeQuery(string query)
+        {
+            try
+            {
+                SQLiteCommand command = new SQLiteCommand(query, dbConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка выполнения запроса \n" + ex.Message);
+            }
+        }
+
+        public DataTable selectData(string query)
+        {
+            try
+            {
+                SQLiteCommand command = new SQLiteCommand(query, dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+
+                DataTable schema = reader.GetSchemaTable();
+                DataTable table = new DataTable();
+
+                //добавление столбцов 
+                if (schema != null)
+                    for (int i = 0; i < schema.Rows.Count; i++)
+                    {
+                        table.Columns.Add(getRusName(schema.Rows[0]["BaseTableName"].ToString(), schema.Rows[i][0].ToString())); 
+                        //table.Columns.Add(schema.Rows[i]["BaseTableName"].ToString()+" "+ schema.Rows[i][0].ToString());
+                    }
+
+                //заполнение строк
+                while (reader.Read())
+                {
+                    DataRow row = table.NewRow();
+                    for (int i = 0; i < table.Columns.Count; i++)
+                        row[i] = reader[i];
+                    table.Rows.Add(row);
+                }
+                return table;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка получения данных \n" + ex.Message);
+            }
+
+        }
+
+        private string getRusName(string table, string engName)
+        {
+            SQLiteCommand command = new SQLiteCommand("SELECT rusName FROM RusNamesFields WHERE engName = '" + table + "." + engName + "'", dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            DataTable schema = reader.GetSchemaTable();
+            string rusName;
+            if (reader.Read())
+                rusName = reader[0].ToString();
+            else
+                rusName = table + " " + engName;
+            return rusName;
+        }
+    }
+}
