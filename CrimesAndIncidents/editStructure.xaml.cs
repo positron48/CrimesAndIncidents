ed7619bd30d3445def9576c8c8917031f104cbd3
positron48 <positron48@gmail.com>
2015-02-05 21:29:52 +0400

    Добавлено окно структуры подразделений
    Хоть что-то, не белый же квадратик оставлять

---
+++ CrimesAndIncidents/editStructure.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CrimesAndIncidents
+{
+    /// <summary>
+    /// Логика взаимодействия для editStructure.xaml
+    /// </summary>
+    public partial class editStructure : Window
+    {
+        public editStructure()
+        {
+            InitializeComponent();
+        }
+    }
+}
