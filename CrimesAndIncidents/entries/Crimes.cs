98e1bffe79616d7b2683c6e9cd35b4cab61a03fd
positron48 <positron48@gmail.com>
2015-02-16 21:27:37 +0400

    Реализовано хранение дат в DateTime

--- CrimesAndIncidents/entries/Crimes.cs
+++ CrimesAndIncidents/entries/Crimes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,14 +45,33 @@ namespace CrimesAndIncidents
         public int IdOrgan { get; set; }
         public int IdClause { get; set; }
         public int IdMilitaryUnit { get; set; }
-        public string DateRegistration { get; set; }
-        public string DateInstitution { get; set; }
-        public string DateCommit { get; set; }
         public string Story { get; set; }
         public string Damage { get; set; }
-        public string DateVerdict { get; set; }
         public string Verdict { get; set; }
         public string NumberCase { get; set; }
+
+        public string DateRegistration {
+            get { return dateRegistration.Year == 9999 ? "" : dateRegistration.ToString("dd.MM.yyyy"); }
+            set { dateRegistration = value != "" ? DateTime.ParseExact(value, "dd.MM.yyyy", CultureInfo.InvariantCulture) : new DateTime(9999, 1, 1); }
+        }
+        public string DateInstitution {
+            get { return dateInstitution.Year == 9999 ? "" : dateInstitution.ToString("dd.MM.yyyy"); }
+            set { dateInstitution = value != "" ? DateTime.ParseExact(value, "dd.MM.yyyy", CultureInfo.InvariantCulture) : new DateTime(9999, 1, 1); }
+        }
+        public string DateCommit {
+            get { return dateCommit.Year == 9999 ? "" : dateCommit.ToString("dd.MM.yyyy"); }
+            set { dateCommit = value != "" ? DateTime.ParseExact(value, "dd.MM.yyyy", CultureInfo.InvariantCulture) : new DateTime(9999, 1, 1); }
+        }
+        public string DateVerdict {
+            get { return dateVerdict.Year == 9999 ? "" : dateVerdict.ToString("dd.MM.yyyy"); }
+            set { dateVerdict = value != "" ? DateTime.ParseExact(value, "dd.MM.yyyy", CultureInfo.InvariantCulture) : new DateTime(9999,1,1); }
+        }
+
+        //ужасный костыль для работы сортировки
+        public DateTime dateRegistration { get; set; }
+        public DateTime dateInstitution { get; set; }
+        public DateTime dateCommit { get; set; }
+        public DateTime dateVerdict { get; set; }
         
         //для отображения в списке
         public string Accomplice { get; set; }
