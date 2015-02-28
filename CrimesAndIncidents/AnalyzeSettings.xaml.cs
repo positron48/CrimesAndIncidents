e53695b0e7e6f0293a6e2c2a406df19758eed1cc
positron48 <positron48@gmail.com>
2015-02-28 18:15:24 +0400

    Дополнения анализа
    анализ по подразделениям
    анализ по участникам

--- CrimesAndIncidents/AnalyzeSettings.xaml.cs
+++ CrimesAndIncidents/AnalyzeSettings.xaml.cs
@@ -110,11 +110,181 @@ namespace CrimesAndIncidents
                         para1.Range.Text = "- войсковая часть " + tableCrimes.Rows[i][0] + " (" + tableCrimes.Rows[i][1] + "): " +
                             tableCrimes.Rows[i][2] + " преступлений, " + tableIncidents.Rows[i][2] + " происшествий";
                         para1.Range.Font.Size = 14;
+                        if (chkOnSubUnit.IsChecked.Value) para1.Range.Font.Bold = 14;
                         para1.Range.InsertParagraphAfter();
+                        if (chkOnSubUnit.IsChecked.Value)
+                        {
+                            DataTable tableSubUnitCrime = sqlWorker.selectData("SELECT Name, COUNT(idCrime) FROM " +
+                                    "(SELECT M.number, M.shortName, S.Name, C.idCrime FROM " +
+                                        "Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime " +
+                                        "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " +
+                                        "LEFT JOIN SubUnit S ON S.idSubUnit = A.idSubUnit  " +
+                                        "LEFT JOIN MilitaryUnit M ON M.idMilitaryUnit = C.idMilitaryUnit " +
+                                    "WHERE C.isRegistred = 1 AND C.dateRegistration  " +
+                                        "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "' AND C.idClause > -1 " +
+                                    "GROUP BY C.idCrime " +
+                                    "ORDER BY M.idMilitaryUnit) " +
+                                "WHERE number = '" + tableCrimes.Rows[i][0] + "' " +
+                                "GROUP BY Name " +
+                                "ORDER BY Name");
+                            for (int j = 0; j < tableSubUnitCrime.Rows.Count; j++)
+                            {
+                                para1.Range.Font.Bold = 0;
+                                para1.Range.Font.Size = 14;
+                                para1.Range.Text = "\t" + (tableSubUnitCrime.Rows[j][0].ToString() == "" ? "не установлено" : tableSubUnitCrime.Rows[j][0].ToString()) + ": " +
+                                    tableSubUnitCrime.Rows[j][1] + " преступлений";
+                                para1.Range.Font.Size = 14;
+                                para1.Range.InsertParagraphAfter();
+                            }
+                        }
                     }
+                    para1.Range.InsertParagraphAfter();
                 }
 
+                if (chkOnAccomplice.IsChecked.Value)
+                {
+                    DataTable tableAccompliceCrime = sqlWorker.selectData("SELECT * FROM " + 
+                        "(SELECT COUNT(A.idAccomplice) as призывники FROM " + 
+                            "Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                            "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                            "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                        "WHERE A.isContrakt = 0  " + 
+                            "AND C.isRegistred = 1  " + 
+                            "AND C.dateRegistration  " + 
+                            "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                            "AND C.idClause > -1), " + 
+                        "(SELECT COUNT(A.idAccomplice) as контрактники FROM " + 
+                            "Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                            "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                            "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                        "WHERE A.isContrakt = 1 " + 
+                            "AND R.priority < 60  " + 
+                            "AND C.isRegistred = 1  " + 
+                            "AND C.dateRegistration  " + 
+                            "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                            "AND C.idClause > -1), " + 
+                        "(SELECT COUNT(A.idAccomplice) as прапорщики FROM " + 
+                            "Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                            "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                            "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                        "WHERE A.isContrakt = 1 " + 
+                            "AND R.priority BETWEEN 60 AND 75 " + 
+                            "AND C.isRegistred = 1  " + 
+                            "AND C.dateRegistration  " + 
+                            "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                            "AND C.idClause > -1), " + 
+                        "(SELECT COUNT(A.idAccomplice) as офицеры FROM " + 
+                            "Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                            "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                            "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                        "WHERE A.isContrakt = 1 " + 
+                            "AND R.priority > 75 " + 
+                            "AND C.isRegistred = 1  " + 
+                            "AND C.dateRegistration  " +
+                            "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                            "AND C.idClause > -1);");
+                    para1.Range.InsertParagraphAfter();
+                    para1.Range.Text = "По участникам: " + Environment.NewLine +
+                        "-солдаты и сержанты по призыву: " + tableAccompliceCrime.Rows[0][0] + Environment.NewLine +
+                        "-солдаты и сержанты по контракту: " + tableAccompliceCrime.Rows[0][1] + Environment.NewLine +
+                        "-прапорщики: " + tableAccompliceCrime.Rows[0][2] + Environment.NewLine +
+                        "-офицеры: " + tableAccompliceCrime.Rows[0][3];
+                    para1.Range.Font.Size = 14;
+                    para1.Range.InsertParagraphAfter();
+                    para1.Range.InsertParagraphAfter();
 
+                    if (chkOnMilitaryUnit.IsChecked.Value)
+                    {
+                        DataTable tableAccompliceMilitaryUnit = sqlWorker.selectData("SELECT M1.number, M1.name, T1.n1 as призывники, T2.n2 as контрактники, T3.n3 as прапорщики, T4.n4 as офицеры FROM  " + 
+                            "(SELECT M.number, M.name FROM MilitaryUnit M) M1 LEFT JOIN  " + 
+                            "(SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n1 FROM " + 
+                                "MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit " + 
+                                "LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                                "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                                "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                            "WHERE A.isContrakt = 0 " + 
+                                "AND C.isRegistred = 1  " + 
+                                "AND C.dateRegistration  " + 
+                                "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                                "AND C.idClause > -1 " + 
+                            "GROUP BY M.idMilitaryUnit " + 
+                            "ORDER BY M.idMilitaryUnit) T1 ON T1.number = M1.number  " + 
+                            "LEFT JOIN " + 
+                            "(SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n2 FROM " + 
+                                "MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit " + 
+                                "LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                                "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                                "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                            "WHERE A.isContrakt = 1 " + 
+                                "AND R.priority < 60 " + 
+                                "AND C.isRegistred = 1  " + 
+                                "AND C.dateRegistration  " + 
+                                "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                                "AND C.idClause > -1 " + 
+                            "GROUP BY M.idMilitaryUnit " + 
+                            "ORDER BY M.idMilitaryUnit) T2 ON T2.number = M1.number  " + 
+                            "LEFT JOIN " + 
+                            "(SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n3 FROM " + 
+                                "MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit " + 
+                                "LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                                "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                                "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                            "WHERE A.isContrakt = 1 " + 
+                                "AND R.priority BETWEEN 60 AND 75 " + 
+                                "AND C.isRegistred = 1  " + 
+                                "AND C.dateRegistration  " + 
+                                "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                                "AND C.idClause > -1 " + 
+                            "GROUP BY M.idMilitaryUnit " + 
+                            "ORDER BY M.idMilitaryUnit) T3  ON T3.number = M1.number  " + 
+                            "LEFT JOIN " + 
+                            "(SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n4 FROM " + 
+                                "MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit " + 
+                                "LEFT JOIN Portaking P ON C.idCrime = P.idCrime " + 
+                                "LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice " + 
+                                "LEFT JOIN Rank R ON R.idRank = A.idRank " + 
+                            "WHERE A.isContrakt = 1 " + 
+                                "AND R.priority > 75 " + 
+                                "AND C.isRegistred = 1  " + 
+                                "AND C.dateRegistration  " +
+                                "BETWEEN '" + dateLeft + "' AND '" + (dateRight == "" ? "9999.99.99" : dateRight) + "'  " + 
+                                "AND C.idClause > -1 " + 
+                            "GROUP BY M.idMilitaryUnit " + 
+                            "ORDER BY M.idMilitaryUnit) T4  ON T4.number = M1.number ");
+
+                        for (int i = 0; i < tableAccompliceMilitaryUnit.Rows.Count; i++)
+                        {
+                            if (tableAccompliceMilitaryUnit.Rows[i][2].ToString() != "" ||
+                                tableAccompliceMilitaryUnit.Rows[i][3].ToString() != "" ||
+                                tableAccompliceMilitaryUnit.Rows[i][4].ToString() != "" ||
+                                tableAccompliceMilitaryUnit.Rows[i][5].ToString() != "")
+                            {
+                                para1.Range.Font.Size = 14;
+                                para1.Range.Font.Bold = 14;
+                                para1.Range.Text = "- войсковая часть " + tableAccompliceMilitaryUnit.Rows[i][0] + " (" + tableAccompliceMilitaryUnit.Rows[i][1] + "): ";
+                                para1.Range.InsertParagraphAfter();
+
+                                para1.Range.Font.Size = 14;
+                                para1.Range.Font.Bold = 0;
+                                para1.Range.Text = (tableAccompliceMilitaryUnit.Rows[i][2].ToString() == "" ?
+                                        "" :
+                                        "\tсолдаты и сержанты по призыву: " + tableAccompliceMilitaryUnit.Rows[i][2] + Environment.NewLine) +
+                                    (tableAccompliceMilitaryUnit.Rows[i][3].ToString() == "" ?
+                                        "" :
+                                        "\tсолдаты и сержанты по контракту: " + tableAccompliceMilitaryUnit.Rows[i][3] + Environment.NewLine) +
+                                    (tableAccompliceMilitaryUnit.Rows[i][4].ToString() == "" ?
+                                        "" :
+                                        "\tпрапорщики: " + tableAccompliceMilitaryUnit.Rows[i][4] + Environment.NewLine) +
+                                    (tableAccompliceMilitaryUnit.Rows[i][5].ToString() == "" ?
+                                        "" :
+                                        "\tофицеры: " + tableAccompliceMilitaryUnit.Rows[i][5]);
+
+                                para1.Range.InsertParagraphAfter();
+                            }
+                        }
+                    }
+
+                }
 
                 winword.Visible = true;
             }
--- CrimesAndIncidents/запросы.sql
+++ CrimesAndIncidents/запросы.sql
@@ -28,3 +28,153 @@ SELECT M.number, M.shortName, C.story FROM
     Crime C ON C.idMilitaryUnit = M.idMilitaryUnit AND C.isRegistred = 1 AND  C.dateRegistration BETWEEN "2014.12.01" AND "2014.12.31"
 --GROUP BY M.number, M.shortName
 ORDER BY M.idMilitaryUnit;
+
+--количество преступлений за период по подразделениям
+SELECT Name, COUNT(idCrime) FROM 
+    (SELECT M.number, M.shortName, S.Name, C.idCrime FROM
+        Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+        LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+        LEFT JOIN SubUnit S ON S.idSubUnit = A.idSubUnit 
+        LEFT JOIN MilitaryUnit M ON M.idMilitaryUnit = C.idMilitaryUnit
+    WHERE C.isRegistred = 1 AND C.dateRegistration 
+        BETWEEN "2014.01.01" AND "2014.12.31" AND C.idClause > -1
+    GROUP BY C.idCrime
+    ORDER BY M.idMilitaryUnit)
+WHERE number = '19612'
+GROUP BY Name
+ORDER BY Name;
+
+--количество происшествий за период по подразделениям
+SELECT Name, COUNT(idCrime) FROM 
+    (SELECT M.number, M.shortName, S.Name, C.idCrime FROM
+        Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+        LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+        LEFT JOIN SubUnit S ON S.idSubUnit = A.idSubUnit 
+        LEFT JOIN MilitaryUnit M ON M.idMilitaryUnit = C.idMilitaryUnit
+    WHERE C.isRegistred = 1 AND C.dateRegistration 
+        BETWEEN "2014.01.01" AND "2014.12.31" AND C.idClause = NULL
+    GROUP BY C.idCrime
+    ORDER BY M.idMilitaryUnit)
+WHERE number = '19612'
+GROUP BY Name
+ORDER BY Name;
+
+--количество призывников преступников
+SELECT COUNT(DISTINCT A.idAccomplice) FROM
+    Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 0 
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1;
+
+--количество с/с контрактников преступников
+SELECT COUNT(DISTINCT A.idAccomplice) FROM
+    Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority < 60 
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1;
+    
+--количество прапорщиков преступников
+SELECT COUNT(DISTINCT A.idAccomplice) FROM
+    Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority BETWEEN 60 AND 75
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1;
+    
+--количество офицеров преступников
+SELECT COUNT(DISTINCT A.idAccomplice) FROM
+    Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority > 75
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1;
+    
+--количество преступлений за период по участникам
+SELECT M.number, M.shortName, R.shortName, R.priority, A.isContrakt, A.shortName, COUNT(C.idCrime) FROM
+    Crime C LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN MilitaryUnit M ON M.idMilitaryUnit = C.idMilitaryUnit
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE C.isRegistred = 1 AND C.dateRegistration BETWEEN "2014.01.01" AND "2014.12.31" AND C.idClause > -1
+GROUP BY A.idAccomplice
+ORDER BY R.priority;
+
+--количество преступлений по частям и участникам
+SELECT M1.number, M1.name, T1.n1 as призывники, T2.n2 as контрактники, T3.n3 as прапорщики, T4.n4 as офицеры FROM 
+(--воинские части
+SELECT M.number, M.name FROM MilitaryUnit M) M1 LEFT JOIN 
+(--количество призывников
+SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n1 FROM
+    MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit
+    LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 0
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1
+GROUP BY M.idMilitaryUnit
+ORDER BY M.idMilitaryUnit) T1 ON T1.number = M1.number 
+LEFT JOIN
+(--количество контрактников
+SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n2 FROM
+    MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit
+    LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority < 60
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1
+GROUP BY M.idMilitaryUnit
+ORDER BY M.idMilitaryUnit) T2 ON T2.number = M1.number 
+LEFT JOIN
+(--количество прапорщиков преступников
+SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n3 FROM
+    MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit
+    LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority BETWEEN 60 AND 75
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1
+GROUP BY M.idMilitaryUnit
+ORDER BY M.idMilitaryUnit) T3  ON T3.number = M1.number 
+LEFT JOIN
+(--количество офицеров преступников
+SELECT M.number, M.name, COUNT(DISTINCT A.idAccomplice) as n4 FROM
+    MilitaryUnit M LEFT JOIN  Crime C On M.idMilitaryUnit = C.idMilitaryUnit
+    LEFT JOIN Portaking P ON C.idCrime = P.idCrime
+    LEFT JOIN Accomplice A ON A.idAccomplice = P.idAccomplice
+    LEFT JOIN Rank R ON R.idRank = A.idRank
+WHERE A.isContrakt = 1
+    AND R.priority > 75
+    AND C.isRegistred = 1 
+    AND C.dateRegistration 
+    BETWEEN "2014.01.01" AND "2014.12.31" 
+    AND C.idClause > -1
+GROUP BY M.idMilitaryUnit
+ORDER BY M.idMilitaryUnit) T4  ON T4.number = M1.number 
